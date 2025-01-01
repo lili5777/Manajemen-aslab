843edc46103fd5a2caacc2fcc7521a8e65b8203f
lili5777 <zailyanzali@gmail.com>
2025-01-01 11:09:38 +0800

    tabel asdos

---
+++ app/Models/Asdos.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Asdos extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'id_user',
+        'id_pendaftar',
+        'nama',
+        'stb',
+        'jurusan',
+        'no_wa',
+        'foto',
+        'skor',
+        'rank'
+    ];
+}
